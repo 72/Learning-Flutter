a0e58007fa18473b59af227d4099a5add30c35a5
Juan Flores Mena <by72mena@gmail.com>
2019-05-25 20:06:25 -0700

    Adding Card UI Demo

--- test_ui/lib/card_ui_page.dart
+++ test_ui/lib/card_ui_page.dart
@@ -7,8 +7,26 @@ class CardUIPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Card UI'),
       ),
-      body: Text('Here'),
+      body: Center(child: CustomCard()),
     );
   }
 }
 
+
+class CustomCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [BoxShadow (color: Colors.black26, blurRadius: 24.0)],
+        borderRadius: BorderRadius.all(Radius.circular(24.0)),
+        ),
+      child: SizedBox(
+        height: 300,
+        width: 240,
+      ),
+    );
+  }
+}
