f197235970bb7e2b18827257dc794d1c4ae6d683
Juan Flores Mena <by72mena@gmail.com>
2019-06-05 21:09:42 -0700

    Adding ListViewBuilder

--- test_ui/lib/appui_page.dart
+++ test_ui/lib/appui_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'data.dart';
 
 class AppUIPage extends StatelessWidget {
   @override
@@ -8,16 +9,58 @@ class AppUIPage extends StatelessWidget {
         title: Text('App UI Demo'),
       ),
       body: Container(
-        padding: EdgeInsets.only(left: 16.0, right: 16.0),
-        child: Center(
-          child: TextFormField(
-            validator: (value){
-              if(value.isEmpty){
-                return "Enter text";
-              }
-            }
-          )
-      ),
+        // padding: EdgeInsets.only(left: 16.0, right: 16.0),
+        child: ListView.builder(
+          itemCount: images.length,
+          scrollDirection: Axis.horizontal,
+          physics: BouncingScrollPhysics(),
+          itemBuilder: (context, index){
+            return CarouselItem(index: index);
+          },
+        ),
     ),);
+  }
+}
+
+class CarouselItem extends StatelessWidget {
+  CarouselItem({this.index});
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 20, bottom: 20, right: 20),
+      child: SizedBox( 
+        width: 230.0,
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 20.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.pink,
+                  boxShadow: [BoxShadow(
+                    color: Colors.black12,
+                    offset: Offset(0.0, 5.0),
+                    blurRadius: 10.0,
+                  )],
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.network(images[0]),
+                ),
+              ],
+            ), 
+          ],
+        ),
+      )
+    );
   }
 }
---
+++ test_ui/lib/data.dart
@@ -0,0 +1,10 @@
+var images = [
+  "https://placeimg.com/640/480/any",
+  "https://placeimg.com/640/480/any",
+  "https://placeimg.com/640/480/any",
+];
+
+var titles = ["First item","Second item","Third item"];
+
+var prices = ["250","125","236"];
+
